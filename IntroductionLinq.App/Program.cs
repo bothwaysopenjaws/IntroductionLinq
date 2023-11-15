0b03a91517db93bedb5fc279226f3e8394744dca
morph <emmanuel.stephant@laposte.net>
2023-11-15 09:51:17 +0100

    Ajoutez des fichiers projet.

---
+++ IntroductionLinq.App/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    /**
+     * Les barmans préparent les boissons
+     * Les serveurs servent les commandes
+     * Un cocktail est composé de plusieurs boissons
+     * Une boisson est contenu dans une bouteille
+     * Les commandes sont associés à des tables
+     * Un verre peut être un cockail ou une boisson simple.
+     * Le prix est au verre.
+     * 
+     */
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
